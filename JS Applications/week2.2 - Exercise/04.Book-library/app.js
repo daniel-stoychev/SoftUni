607fa37b87667fad3e7ced46a2c8f1dc23193467
Daniel Stoychev <daniel.stoychev@Daniel-Stoychev.local>
2025-01-26 22:40:05 +0200

    04 completed

--- JS Applications/week2.2 - Exercise/04.Book-library/app.js
+++ JS Applications/week2.2 - Exercise/04.Book-library/app.js
@@ -2,9 +2,12 @@ const requstURL = 'http://localhost:3030/jsonstore/collections/books';
 const loadAllBooks = document.getElementById('loadBooks');
 loadAllBooks.addEventListener('click', fetchBooks);
 const tableData = document.querySelector('tbody');
-
 const createBookForm = document.querySelector('form');
+const formButton = document.querySelector('form button');
+
+
 createBookForm.addEventListener('submit', createBook);
+
 
 async function fetchBooks() {
     tableData.innerHTML = '';
@@ -13,6 +16,9 @@ async function fetchBooks() {
     console.log(data);
 
     Object.entries(data).forEach(([id, bookInfo]) => {
+
+        console.log(bookInfo);
+
 
         const tableRow = document.createElement('tr');
         const authorNameCell = document.createElement('td');
@@ -27,10 +33,10 @@ async function fetchBooks() {
         deleteBnt.textContent = 'Delete';
         buttonsCell.append(editBnt, deleteBnt);
 
-        tableRow.append(authorNameCell, bookNameCell, buttonsCell);
+        tableRow.append(bookNameCell, authorNameCell, buttonsCell);
         tableData.appendChild(tableRow);
 
-        // editBnt.addEventListener('click', () => editBook(id));
+        editBnt.addEventListener('click', () => obtainBookDetails(id));
 
         deleteBnt.addEventListener('click', () => deleteBook(id));
 
@@ -38,6 +44,49 @@ async function fetchBooks() {
     })
 
 
+}
+
+function obtainBookDetails(id) {
+    const formHeading = document.querySelector('form h3');
+    formHeading.textContent = 'Edit FORM';
+
+
+    formButton.textContent = 'Save';
+
+    const bookTitle = document.querySelector('input[name="title"]');
+    const bookAuthor = document.querySelector('input[name="author"]');
+
+    fetch(`${requstURL}/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            bookTitle.value = data.title;
+            bookAuthor.value = data.author;
+
+            const newFormButton = formButton.cloneNode(true);
+            formButton.parentNode.replaceChild(newFormButton, formButton);
+
+
+            newFormButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                editBook(id);
+            });
+        });
+}
+
+async function editBook(id) {
+    const bookTitle = document.querySelector('input[name="title"]');
+    const bookAuthor = document.querySelector('input[name="author"]');
+    const updateBook = await fetch((`${requstURL}/${id}`), {
+        method: 'PUT',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            author: bookAuthor.value,
+            title: bookTitle.value
+        })
+    })
+    fetchBooks();
 }
 
 function deleteBook(id) {
@@ -72,6 +121,7 @@ async function createBook(e) {
 
 
     } else {
+        alert('Empty fields')
         throw new Error("Empty fields");
 
     }
