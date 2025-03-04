a81b14520778dba77fc7c061d2195a9e54ec45e6
Daniel Stoychev <daniel.stoychev@Daniel_Stoychev.sgnet.lan>
2025-03-04 14:28:42 +0200

    remain adding matches

--- JSApplications/week3.4 - Exercise/03.Search-in-List/search.js
+++ JSApplications/week3.4 - Exercise/03.Search-in-List/search.js
@@ -1,3 +1,31 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js"
+
+const townsEl = document.querySelector('#towns');
+const searchBtn = document.querySelector('button');
+searchBtn.addEventListener('click', search)
+
 function search() {
-   // TODO
+   // console.log('TEST');
+   const searchBoxValue = document.querySelector('#searchText').value;
+   const match = towns.filter(town => town.includes(searchBoxValue));
+   console.log(match);
+
+   const resultEl = document.querySelector('#result');
+   resultEl.textContent = `${match.length} matches found`;
 }
+
+function townsTemplate() {
+   return html`
+      <ul>
+         ${towns.map(town =>
+      html`
+            <li>${town}</li>
+            `
+   )
+      }
+      </ul >
+   `;
+}
+
+render(townsTemplate(), townsEl)
