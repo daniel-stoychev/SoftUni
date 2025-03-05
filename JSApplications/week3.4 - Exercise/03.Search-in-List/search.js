88890e55f698940c2cf3104e48e700ce0f1dd2d2
Daniel Stoychev <daniel.stoychev@Daniel-Stoychev.local>
2025-03-05 16:18:09 +0200

    03 task done

--- JSApplications/week3.4 - Exercise/03.Search-in-List/search.js
+++ JSApplications/week3.4 - Exercise/03.Search-in-List/search.js
@@ -7,25 +7,23 @@ searchBtn.addEventListener('click', search)
 
 function search() {
    // console.log('TEST');
-   const searchBoxValue = document.querySelector('#searchText').value;
-   const match = towns.filter(town => town.includes(searchBoxValue));
-   console.log(match);
-
+   const searchBoxValue = document.querySelector('#searchText').value.toUpperCase();
+   const match = towns.filter(town => town.toUpperCase().includes(searchBoxValue));
    const resultEl = document.querySelector('#result');
    resultEl.textContent = `${match.length} matches found`;
+   render(townsTemplate(searchBoxValue), townsEl);
 }
 
-function townsTemplate() {
+function townsTemplate(searchTerm = '') {
    return html`
       <ul>
-         ${towns.map(town =>
-      html`
-            <li>${town}</li>
-            `
-   )
-      }
-      </ul >
+         ${towns.map(town => {
+      const isActive = searchTerm && town.toUpperCase().includes(searchTerm) ? 'active' : '';
+      return html`
+               <li class="${isActive}">${town}</li>
+            `;
+   })}
+      </ul>
    `;
 }
-
 render(townsTemplate(), townsEl)
