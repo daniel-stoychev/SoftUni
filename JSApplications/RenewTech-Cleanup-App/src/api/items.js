698a341d763dea76ce95828d33b732d3ebd63ebd
Daniel Stoychev <daniel.stoychev@Daniel-Stoychev.local>
2025-04-06 13:10:07 +0300

    exam prep

--- JSApplications/RenewTech-Cleanup-App/src/api/items.js
+++ JSApplications/RenewTech-Cleanup-App/src/api/items.js
@@ -11,3 +11,9 @@ export const createItem = (data) => request('POST', `${baseUrl}/solutions`, data
 export const deleteItem = (id) => request('DELETE', `${baseUrl}/solutions/${id}`);
 
 export const updateItem = (id, data) => request('PUT', `${baseUrl}/solutions/${id}`, data);
+
+export const likeItemRequest = (data) => request('POST', `${baseUrl}/likes`, data);
+
+export const getAllLikes = (solutionId) => request('GET', `${baseUrl}/likes?where=solutionId%3D%22${solutionId}%22&distinct=_ownerId&count`);
+
+export const getLikesForUser = (solutionId, userId) => request('GET', `${baseUrl}/likes?where=solutionId%3D%22${solutionId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
--- JSApplications/RenewTech-Cleanup-App/src/views/details copy.js
+++ JSApplications/RenewTech-Cleanup-App/src/views/details copy.js
@@ -1,11 +1,10 @@
 
-import { getAllLikes, getLikesForUser, getOneItem, likeItemRequest } from '../api/items.js';
+import { getOneItem } from '../api/items.js';
 import { html, render } from '../lib/lit-html.js';
-import page from '../lib/page.js';
 import { getUserData } from '../utils/userUrils.js';
 
 
-const template = (itemDetails, isOwner, hasUser, likesCount, hasLiked) => html`
+const template = (itemDetails, isOwner, hasUser) => html`
     <section id="details">
         <div id="details-wrapper">
             <img id="details-img" src=${itemDetails.imageUrl} alt="example1" />
@@ -21,7 +20,7 @@ const template = (itemDetails, isOwner, hasUser, likesCount, hasLiked) => html`
                         </p>
                     </div>
                     </div>
-                    <h3>Like Solution:<span id="like">${likesCount}</span></h3>
+                    <h3>Like Solution:<span id="like">0</span></h3>
 
  
                         <!--Edit and Delete are only for creator-->
@@ -30,8 +29,8 @@ const template = (itemDetails, isOwner, hasUser, likesCount, hasLiked) => html`
         ? html`
                         <a href="/edit/${itemDetails._id}" id="edit-btn">Edit</a>
                         <a href="/delete/${itemDetails._id}" id="delete-btn">Delete</a>`
-        : !isOwner && hasUser && !hasLiked ? html`<!--Bonus - Only for logged-in users ( not authors )-->
-                            <a href="/like/${itemDetails._id}" id="like-btn">Like</a>` : ''}
+        : !isOwner && hasUser ? html`<!--Bonus - Only for logged-in users ( not authors )-->
+                            <a href="#" id="like-btn">Like</a>` : ''}
                 </div>
             </div>
         </div>
@@ -52,22 +51,6 @@ export default async function detailsView(ctx) {
     if (id !== null) {
         hasUser = true;
     }
-
-
-    const itemID = itemDetails._id;
-
-    //all likes count
-    const likesCount = await getAllLikes(itemID);
-    // get likes for user
-    const hasLiked = await getLikesForUser(itemID, id)
-
-    render(template(itemDetails, isOwner, hasUser, likesCount, hasLiked));
-}
-
-export async function likeItem(ctx) {
-    const solutionId = ctx.params.itemId;
-    await likeItemRequest({ solutionId });
-    page.redirect(`/dashboard/${ctx.params.itemId}`);
-
+    render(template(itemDetails, isOwner, hasUser));
 }
 
