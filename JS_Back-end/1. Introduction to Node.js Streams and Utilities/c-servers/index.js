9eba16f966feb9dbf0bfbeb9c84a3e3b300589fd
Daniel Stoychev <daniel.stoychev@Daniel-Stoychev.local>
2025-09-14 14:24:40 +0300

    web server

--- JS_Back-end/1. Introduction to Node.js Streams and Utilities/c-servers/index.js
+++ JS_Back-end/1. Introduction to Node.js Streams and Utilities/c-servers/index.js
@@ -1,20 +1,70 @@
 import http from "http";
+import url from 'url';
 
 const server = http.createServer((req, res) => {
     console.log('Request Received');
 
-    const url = new URL('https://en.wikipedia.org/wiki/HTTP#History');
-    const searchParams = new URLSearchParams(req.url.split('?')[1]);
+    console.log('HTTP Method:', req.method);
+
+
+    const paresedUrl = url.parse(req.url);
+
+    console.log(paresedUrl);
+
+
+    // const url = new URL('https://en.wikipedia.org/wiki/HTTP#History');
+    const searchParams = new URLSearchParams(paresedUrl.query);
     console.log(searchParams);
 
 
 
+    if (paresedUrl.pathname === '/') {
+        //This is root page 
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+        res.write(`
+            <html>
+                <head>
+                   <link rel="stylesheet" href="/css/style.css"> 
+                </head>
+                <body>
+                    <h1>Hello from my first HTTP server</h1>
+                </body>
+                
+            </html>
+            `);
 
-    res.writeHead(200, {
-        'content-type': 'text/html'
-    })
-    res.write('<h1>Hello from my first HTTP server</h1>');
+    } else if (paresedUrl.pathname === '/cats') {
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+        res.write(`
+            <html>
+                <head>
+                   <link rel="stylesheet" href="/css/style.css"> 
+                </head>
+                <body>
+                    <h1>This is Cats page</h1>
+                </body>
+                
+            </html>
+            `)
+    } else if (paresedUrl.pathname === '/css/style.css') {
+        res.writeHead(200, {
+            'content-type': 'text/css'
+        });
+        res.write(`
+                body {
+                    background-color: aquamarine;
+                    }
+            `);
+    }
+
+
     res.end();
+
+
 });
 
 server.listen(5000);
