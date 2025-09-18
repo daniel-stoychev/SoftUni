416507fd88bbb0520434ddc916b9f1ac7244e03a
Daniel Stoychev <daniel.stoychev@Daniel-Stoychev.local>
2025-09-18 23:23:22 +0300

    cat shelter project

--- JS_Back-end/1.2 Exercise/src/index.js
+++ JS_Back-end/1.2 Exercise/src/index.js
@@ -1,24 +1,55 @@
-console.log('It works!');
 import http from 'http';
-import homeHtml from './home.html.js';
-import homeCSS from './home.css.js';
+import fs from 'fs/promises';
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        });
-        res.write(homeHtml);
-    } else if (req.url === '/styles/site.css') {
-        res.writeHead(200, {
-            "content-type": 'text/css'
-        });
-        res.write(homeCSS);
+const server = http.createServer(async (req, res) => {
+    let html;
+    switch (req.url) {
+        case '/':
+            html = await homeView();
+            break;
+        case '/cats/add-breed':
+            html = await addBreedView();
+            break;
+        case '/cats/add-cat':
+            html = await addCatView();
+            break;
+
+
+        case '/styles/site.css':
+            const siteCss = await fs.readFile('./src/styles/site.css', { encoding: 'utf-8' });
+
+            res.writeHead(200, {
+                "content-type": 'text/css'
+            });
+            res.write(siteCss);
+            return res.end();
+        default:
+            return res.end();
+
     }
 
-
+    res.writeHead(200, {
+        "content-type": "text/html"
+    })
+    res.write(html);
     res.end();
 });
+
+
+async function homeView() {
+    const homeHtml = await fs.readFile('./src/views/home/index.html', { encoding: 'utf-8' });
+    return homeHtml;
+}
+
+async function addBreedView() {
+    const addBreedPage = await fs.readFile('./src/views/addBreed.html', { encoding: 'utf-8' });
+    return addBreedPage;
+}
+
+async function addCatView() {
+    const addCatPage = await fs.readFile('./src/views/addCat.html', { encoding: 'utf-8' });
+    return addCatPage;
+}
 
 server.listen(5000);
 console.log('Server is listening on http://localhost:5000...');
