e76d9b99ccadf642d9ccbe840df8c4bf42e8ba14
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-10-16 15:38:31 +0300

    fix own property handlebars problem with lean method

--- JS_Back-end/movie-magic-project/src/services/movieService.js
+++ JS_Back-end/movie-magic-project/src/services/movieService.js
@@ -1,8 +1,12 @@
 import Movie from "../models/Movie.js";
 
 export default {
-    getAll(filter) {
-        return Movie.find(filter);
+    async getAll(filter) {
+        const result = await Movie.find(filter).lean();
+        //await Movie.find(filter) - query of document
+        // lean() makes object from document and resilves "Handlebars: Access has been denied to resolve the property" error
+
+        return result;
     },
 
     getOne(id) {
