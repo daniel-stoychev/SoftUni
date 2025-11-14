d2325bcd63c79ef21ae3984ff0334f78db74a4db
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-11-14 23:48:00 +0200

    my first JSX App

--- React/JSX_Demo/dist/App.js
+++ React/JSX_Demo/dist/App.js
@@ -1,8 +1,23 @@
 import React from "https://esm.sh/react@19.2.0";
 
-var h1ReactElement = React.createElement('h1', null, 'My first ReactApp');
-var h2ReactElement = React.createElement('h2', null, 'The best framework ever!');
+// const h1ReactElement = React.createElement('h1', null, 'My first ReactApp');
+// const h2ReactElement = React.createElement('h2', null, 'The best framework ever!');
 
-var App = React.createElement('div', { calssName: 'app-container' }, null, h1ReactElement, h2ReactElement);
+// const AppJSX = React.createElement('div', { calssName: 'app-container' }, null, h1ReactElement, h2ReactElement)
 
-export default App;
+var AppJSX = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "My first JSX App!"
+    ),
+    React.createElement(
+        "h2",
+        null,
+        "The best framework ever!"
+    )
+);
+
+export default AppJSX;
--- React/JSX_Demo/src/App.js
+++ React/JSX_Demo/src/App.js
@@ -1,12 +1,18 @@
 import React from "https://esm.sh/react@19.2.0";
 
-const h1ReactElement = React.createElement('h1', null, 'My first ReactApp');
-const h2ReactElement = React.createElement('h2', null, 'The best framework ever!');
+// const h1ReactElement = React.createElement('h1', null, 'My first ReactApp');
+// const h2ReactElement = React.createElement('h2', null, 'The best framework ever!');
 
-const App = React.createElement('div', { calssName: 'app-container' }, null, h1ReactElement, h2ReactElement)
+// const AppJSX = React.createElement('div', { calssName: 'app-container' }, null, h1ReactElement, h2ReactElement)
+
+const AppJSX = (
+    <div>
+        <h1>My first JSX App!</h1>
+        <h2>The best framework ever!</h2>
+    </div>
+
+);
 
 
-
-
-export default App;
+export default AppJSX;
 
