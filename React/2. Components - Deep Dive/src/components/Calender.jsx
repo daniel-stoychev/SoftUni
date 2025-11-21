b12cb5efdf297f3a77798128f2e53d41d33788e4
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-11-22 00:47:59 +0200

    conditional rendering with if/switch

--- React/2. Components - Deep Dive/src/components/Calender.jsx
+++ React/2. Components - Deep Dive/src/components/Calender.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-const days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
+// const days = [
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+//   "Sunday",
+// ];
 
 export default function Calendar() {
   const [day, setDay] = useState(0);
@@ -18,6 +18,35 @@ export default function Calendar() {
   const resetDayClickHandler = () => {
     setDay(0);
   };
+
+  let currentDay;
+
+  switch (day) {
+    case 0:
+      currentDay = <strong>Monday</strong>;
+      break;
+    case 1:
+      currentDay = <strong>Tuesday</strong>;
+      break;
+    case 2:
+      currentDay = <strong>Wednesday</strong>;
+      break;
+    case 3:
+      currentDay = <strong>Thursday</strong>;
+      break;
+    case 4:
+      currentDay = <strong>Friday</strong>;
+      break;
+    case 5:
+      currentDay = <strong>Saturday</strong>;
+      break;
+    case 6:
+      currentDay = <strong>Sunday</strong>;
+      break;
+
+    default:
+      break;
+  }
 
   if (day > 6) {
     return (
@@ -34,7 +63,8 @@ export default function Calendar() {
   return (
     <section>
       <h1>Calendar</h1>
-      <p>Current Day {days[day]}</p>
+      {/* <p>Current Day {days[day]}</p> */}
+      <p>Current Day {currentDay}</p>
       <button onClick={nextDayClickHandler}>Next day</button>
     </section>
   );
