6e5bf264b3fd9eede037dbf2ca4bb2132225a3cc
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-11-24 05:13:24 +0200

    show create user Modal

--- React/2.1 Exercise/client/src/App.jsx
+++ React/2.1 Exercise/client/src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import Pagination from "./components/Pagination.jsx";
 import Search from "./components/Search.jsx";
 import UserList from "./components/UserList.jsx";
+import CreateUser from "./components/CreateUser.jsx";
 
 function App() {
+  const [showCreateUser, setShowCreateUser] = useState(false);
+  function addUserClickHandler() {
+    setShowCreateUser(true);
+  }
   return (
     <div>
       <Header />
@@ -12,9 +18,12 @@ function App() {
         <section className="card users-container">
           <Search />
           <UserList />
-          <button className="btn-add btn">Add new user</button>
+          <button className="btn-add btn" onClick={addUserClickHandler}>
+            Add new user
+          </button>
           <Pagination />
         </section>
+        {showCreateUser && <CreateUser />};
       </main>
       <Footer />
     </div>
