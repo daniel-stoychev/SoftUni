735d8b6b724d6569c31d11b2d1ac15911b777e49
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-11-24 22:04:10 +0200

    show dynamic user details

--- React/2.1 Exercise/client/src/components/UserDetails.jsx
+++ React/2.1 Exercise/client/src/components/UserDetails.jsx
@@ -1,18 +1,29 @@
-export default function UserDetail({ onClose }) {
+import { useEffect, useState } from "react";
+import { fromIsoDate } from "../utils/datetimeUtils.js";
+
+export default function UserDetail({ onClose, userId }) {
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:3030/jsonstore/users/${userId}`)
+      .then((response) => response.json())
+      .then((result) => {
+        setUserData(result);
+      });
+  }, [userId]);
   return (
-    <div class="overlay">
-      <div class="backdrop" onClick={onClose}></div>
-      <div class="modal">
-        <div class="detail-container">
-          <header class="headers">
+    <div className="overlay">
+      <div className="backdrop" onClick={onClose} />
+      <div className="modal">
+        <div className="detail-container">
+          <header className="headers">
             <h2>User Detail</h2>
-            <button class="btn close" onClick={onClose}>
+            <button className="btn close" onClick={onClose}>
               <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fas"
                 data-icon="xmark"
-                class="svg-inline--fa fa-xmark"
+                className="svg-inline--fa fa-xmark"
                 role="img"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
@@ -20,42 +31,48 @@ export default function UserDetail({ onClose }) {
                 <path
                   fill="currentColor"
                   d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
-                ></path>
+                />
               </svg>
             </button>
           </header>
-          <div class="content">
-            <div class="image-container">
+          <div className="content">
+            <div className="image-container">
               <img
-                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-                alt=""
-                class="image"
+                src={userData.imageUrl}
+                alt={`${firstName}'s profile`}
+                className="image"
               />
             </div>
-            <div class="user-details">
+            <div className="user-details">
               <p>
-                User Id: <strong>62bb0c0eda039e2fdccba57b</strong>
+                User Id: <strong>{userData._id}</strong>
               </p>
               <p>
                 Full Name:
-                <strong> Peter Johnson </strong>
+                <strong>
+                  {" "}
+                  {userData.firstnName} {userData.lastName}{" "}
+                </strong>
               </p>
               <p>
-                Email: <strong>peter@abv.bg</strong>
+                Email: <strong>{userData.email}</strong>
               </p>
               <p>
-                Phone Number: <strong>0812345678</strong>
+                Phone Number: <strong>{userData.phoneNumber}</strong>
               </p>
               <p>
                 Address:
-                <strong> Bulgaria, Sofia, Aleksandar Malinov 78 </strong>
+                <strong>
+                  {" "}
+                  {userData.address?.country} {userData.address?.city}{" "}
+                  {userData.address?.street} {userData.address?.streeNUmber}{" "}
+                </strong>
               </p>
-
               <p>
-                Created on: <strong>Wednesday, June 28, 2022</strong>
+                Created on: <strong>{fromIsoDate(userData.createdAt)}</strong>
               </p>
               <p>
-                Modified on: <strong>Thursday, June 29, 2022</strong>
+                Modified on: <strong>{fromIsoDate(userData.updatedAt)}</strong>
               </p>
             </div>
           </div>
