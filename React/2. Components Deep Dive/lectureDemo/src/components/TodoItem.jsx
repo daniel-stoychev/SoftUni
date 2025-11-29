860950087298da607f0e7a583d813f32bc0fd098
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-11-29 17:18:46 +0200

    complete toggle functionality applied

--- React/2. Components Deep Dive/lectureDemo/src/components/TodoItem.jsx
+++ React/2. Components Deep Dive/lectureDemo/src/components/TodoItem.jsx
@@ -1,5 +1,14 @@
 import styles from "./TodoItem.module.css";
 
-export default function TodoItem({ _id, title, completed }) {
-  return <li className={completed && styles["todo-completed"]}>{title}</li>;
+export default function TodoItem({ id, title, completed, onClick }) {
+  const classes = [styles["pointer"]];
+  if (completed) {
+    classes.push(styles["todo-completed"]);
+  }
+
+  return (
+    <li onClick={() => onClick(id)} className={classes.join(" ")}>
+      {title}
+    </li>
+  );
 }
--- React/2. Components Deep Dive/lectureDemo/src/components/TodoList.jsx
+++ React/2. Components Deep Dive/lectureDemo/src/components/TodoList.jsx
@@ -3,6 +3,7 @@ import TodoItem from "./TodoItem.jsx";
 
 export default function TodoList() {
   const [todos, setTodods] = useState([]);
+  const [refresh, setRefresh] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3030/jsonstore/todos")
       .then((response) => response.json())
@@ -11,7 +12,27 @@ export default function TodoList() {
         console.log(Object.values(result));
       })
       .catch((err) => alert(err.message));
-  }, []);
+  }, [refresh]);
+
+  const completeTaskHandler = (todoId) => {
+    console.log(todoId);
+    const currentTodo = todos.find((todo) => todo._id === todoId);
+    currentTodo.completed = !currentTodo.completed;
+    fetch(`http://localhost:3030/jsonstore/todos/${currentTodo._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(currentTodo),
+    })
+      .then(() => {
+        console.log(currentTodo);
+        setRefresh((state) => !state);
+        console.log("Done!");
+      })
+      .catch((err) => alert(err.message));
+  };
+
   return (
     <ul>
       {todos.map((todo) => (
@@ -19,6 +40,8 @@ export default function TodoList() {
           key={todo._id}
           title={todo.title}
           completed={todo.completed}
+          id={todo._id}
+          onClick={completeTaskHandler}
         />
       ))}
     </ul>
