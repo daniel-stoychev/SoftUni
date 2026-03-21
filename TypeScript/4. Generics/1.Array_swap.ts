93dc5f058660be32a1dde0adb412c93d498435c1
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-03-21 11:20:54 +0200

    simple generic

---
+++ TypeScript/4. Generics/1.Array_swap.ts
@@ -0,0 +1,17 @@
+function swap<T>(
+    a: T[],
+    aIndex: number,
+    b: T[],
+    bIndex: number
+): void {
+    const tempArr = a[aIndex];
+    a[aIndex] = b[bIndex];
+    b[bIndex] = tempArr;
+}
+
+
+let a = [20, 30 , 40];
+let b = [1, 2, 3, 4, 5];
+swap<number>(a, 0, b, 2);
+console.log(a)
+console.log(b)
