3624f63997a4fa1caee66b3c054a72f0ff3e687c
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-03-22 15:16:29 +0200

    type that extracts from abother type, 2nd generic param exatract keys from 1sr

---
+++ TypeScript/4. Generics/6.Choose_type.ts
@@ -0,0 +1,26 @@
+type Choose<T, K extends keyof T> = {
+    [sth in K]: T[sth]
+}
+
+
+
+// type test = { 
+//    name: string,
+//    age: number,
+//    test:() => string;
+// }
+// type extracted = Choose<test, 'name' | 'age'>
+
+type anotherType = {
+    time: Date,
+    duration: number,
+    test: () => string,
+    val: 200 | 300,
+    user: {
+        name: string,
+        age: number
+    }
+}
+
+type nestedUserAndTime = 'user' | 'time'
+type extracted2 = Choose<anotherType, nestedUserAndTime>
