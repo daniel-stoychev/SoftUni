694e70609973170a119a22d308086355b340ea0d
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-04-01 15:28:47 +0300

    add get decortor

---
+++ TypeScript/Examp Prep/src/decorators.ts
@@ -0,0 +1,26 @@
+export function ConvertToEuro(
+    target: object,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalGetter = descriptor.get;
+
+    if (!originalGetter) {
+        throw new Error("ConvertToEuro can only be applied to getters");
+    }
+
+    descriptor.get = function () {
+        const result = originalGetter.call(this);
+
+        
+        if (result === undefined) {
+            return undefined;
+        }
+
+        const eur = result / 1.95583;
+
+        return Number(eur.toFixed(2));
+    };
+
+    return descriptor;
+}
