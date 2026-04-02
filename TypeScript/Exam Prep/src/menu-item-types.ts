e2a9f723134da6c2ac857053581f2b2c283fc585
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-04-02 13:19:18 +0300

    update root folder name

---
+++ TypeScript/Exam Prep/src/menu-item-types.ts
@@ -0,0 +1,74 @@
+import { ConvertToEuro } from "./decorators";
+import { MenuItem, MenuItemType, WithId } from "./models";
+
+abstract class BaseMenuItem implements MenuItem{
+    id: number;
+    name: string;
+    weightGrams: number;
+    type: MenuItemType;
+    constructor(id: number, name: string, weightGrams: number, type: MenuItemType) {
+        this.id = id;
+        this.name = name;
+        this.weightGrams = weightGrams;
+        this.type = type;
+    }
+
+    abstract getCalories():number;
+
+    protected _basePrice?: number;
+    get basePrice(): number | undefined {
+        return this._basePrice;
+    }
+
+    @ConvertToEuro 
+    get finalPrice(): number | undefined { return this._basePrice;}
+}
+
+class WelcomeSnack extends BaseMenuItem {
+    hasCream: boolean;
+    constructor(id: number, name: string, weightGrams: number, hasCream: boolean) {
+        super(id, name, weightGrams, MenuItemType.WelcomeSnack);
+        this.hasCream = hasCream;
+    }
+
+    getCalories(): number {
+        return (this.weightGrams * 1.2) + (this.hasCream ? 20 : 0);
+    }
+}
+
+class MainCourse extends BaseMenuItem {
+    fatGrams: number;
+    constructor(id: number, name: string, weightGrams: number, fatGrams: number, basePrice: number) {
+        super(id, name, weightGrams, MenuItemType.MainCourse);
+        this.fatGrams = fatGrams;
+        this._basePrice = basePrice;
+    }
+    getCalories(): number {
+        return (this.weightGrams * 2.0) + (this.fatGrams * 3);
+    }
+}
+
+class Dessert extends BaseMenuItem {
+    hasSugar: boolean;
+    constructor(id: number, name: string, weightGrams: number, hasSugar: boolean, basePrice: number) {
+        super(id, name, weightGrams, MenuItemType.Dessert);
+        this.hasSugar = hasSugar;
+        this._basePrice = basePrice;
+    }
+    getCalories(): number {
+        return (this.weightGrams * 2.5) + (this.hasSugar ? 100 : 0);
+    }
+}
+
+function findItemById<T extends WithId> (items: T[], id: number): T | undefined {
+    const itemFound = items.find((el) => el.id === id)
+    return itemFound;
+}
+
+export {
+    BaseMenuItem,
+    WelcomeSnack,
+    MainCourse,
+    Dessert,
+    findItemById
+}
