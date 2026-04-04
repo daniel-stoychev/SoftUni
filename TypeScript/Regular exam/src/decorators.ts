86474fc07ce0930f635469bf8dabf35d98cd478c
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-04-04 11:20:26 +0300

    decorator completed

---
+++ TypeScript/Regular exam/src/decorators.ts
@@ -0,0 +1,22 @@
+export function ApplyInsurance(target: object, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalGet = descriptor.get;
+
+    if (!originalGet) {
+        throw new Error("ApplyInsurance can only be applied to getters");
+    }
+
+    descriptor.get = function () {
+        const result = originalGet.call(this);
+
+        if (result === undefined) {
+            return undefined;
+        }
+
+        const surchargePrice = (result * 1.12).toFixed(2);
+
+        return surchargePrice;
+    }
+
+    return descriptor;
+
+}
